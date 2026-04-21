2d49adf484a789c5e09588ac14b658373fc905d3
Fatima Pura <140367080+st-f4tima@users.noreply.github.com>
2026-04-21 21:58:04 +0800

    feat: implement user class

--- TriQue/Models/User.cs
+++ TriQue/Models/User.cs
@@ -1,10 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using TriQue.Enums;
 
 namespace TriQue.Models
 {
-    internal class User
+    public abstract class User
     {
+        public required int UserID { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string PhoneNumber { get; set; }
+        public required UserRole Role { get; set; }
+        public required int FailedAttempts { get; set; } = 0;
+        public DateTime? LockoutUntil { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
