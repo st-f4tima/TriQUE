a2b89818beeda2632f7d6c090259720538929740
Fatima Pura <140367080+st-f4tima@users.noreply.github.com>
2026-04-22 12:53:05 +0800

    fix: clean up project structure and delete unnecessary files

--- TriQue/Forms/LoginForm.cs
+++ TriQue/Forms/LoginForm.cs
@@ -1,10 +1,40 @@
-namespace TriQue
+namespace TriQue.Forms
 {
     public partial class LoginForm : Form
     {
         public LoginForm()
         {
             InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBoxShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = !checkBoxShowPassword.Checked;
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+            textBoxPassword.UseSystemPasswordChar = true;
         }
     }
 }
