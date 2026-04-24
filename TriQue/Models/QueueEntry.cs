44864ea991eb6b6447c1f9bf47b04791a235c63b
Fatima Pura <140367080+st-f4tima@users.noreply.github.com>
2026-04-24 23:48:51 +0800

    feature: add QueueEntry, Route, and Trip models

---
+++ TriQue/Models/QueueEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriQue.Models
+{
+    public class QueueEntry
+    {
+        public int EntryID { get; set; }
+        public int QueueID { get; set; }
+        public int DriverID { get; set; }
+        public int QueuePosition { get; set; }
+        public DateTime JoinedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ TriQue/Models/Route.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriQue.Models
+{
+    public class Route
+    {
+        public int RouteID { get; set; }
+        public string RouteName { get; set; } = string.Empty;
+        public int AssignedGroupID { get; set; }
+
+    }
+}
