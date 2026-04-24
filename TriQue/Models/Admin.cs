5bcf99b0d86aa350d67643f1c1dd11d0566d0151
Fatima Pura <140367080+st-f4tima@users.noreply.github.com>
2026-04-24 23:30:38 +0800

    feat: add AdminID field in Admin class and create DriverGroup and Queue models

--- TriQue/Models/Admin.cs
+++ TriQue/Models/Admin.cs
@@ -5,6 +5,7 @@ namespace TriQue.Models
 {
     public class Admin : User
     {
+        public int AdminID { get; set; }
         public AdminLevel Level { get; set; }
 
     }
---
+++ TriQue/Models/DriverGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriQue.Models
+{
+    public class DriverGroup
+    {
+        public int GroupID { get; set; }
+        public string GroupName { get; set; } = string.Empty;
+        public int RotationDay { get; set; }
+
+    }
+}
